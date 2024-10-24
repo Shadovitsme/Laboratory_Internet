19b5cfdc4fca9017d1ed89f06ab138ceafaa1f78
ukki <iu.cashirskaia@yandex.ru>
2024-10-24 14:33:43 +0300

    Базовая форма для реализации апи

--- index.php
+++ index.php
@@ -1,2 +1,50 @@
-<?php
-echo 'hi';
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form id='userAdd'>
+            <input type="text" placeholder="login">
+            <input type="password" placeholder="password">
+            <input type="button" value="registration">
+        </form>
+    </div>
+
+    <div>
+        <form id='auth'>
+            <input type="text" placeholder="login">
+            <input type="password" placeholder="password">
+            <input type="button" value="auth">
+        </form>
+    </div>
+
+    <div>
+        <form id='update'>
+            <input type="text" placeholder="login">
+            <input type="password" placeholder="password">
+            <input type="button" value="update">
+        </form>
+    </div>
+
+    <div>
+        <form id='delete'>
+            <input type="text" placeholder="login">
+            <input type="password" placeholder="password">
+            <input type="button" value="delete">
+        </form>
+    </div>
+
+    <div>
+        <form id='getdata'>
+            <input type="text" placeholder="login">
+            <input type="button" value="getinf0">
+        </form>
+    </div>
+</body>
+
+</html>
